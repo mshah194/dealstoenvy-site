152fed28e6c4a1a1bdec0f8dcd4eda32e7a144f1
mshah194 <shah194@gmail.com>
2026-03-29 12:46:54 -0500

    Refactor sheet config to use window.DEALS_SHEET_CONFIG

--- assets/js/sheet-config.js
+++ assets/js/sheet-config.js
@@ -1,1 +1,6 @@
-const SHEET_CSV_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vSELfNjkuSfkGPyyiNGJKY0H9A6nH3nDwhOmN6mp0Ne4wS4cyP6TAev6nu84RZXAA/pub?gid=1791302643&single=true&output=csv";
+window.DEALS_SHEET_CONFIG = {
+  csvUrl: "https://docs.google.com/spreadsheets/d/e/2PACX-1vSELfNjkuSfkGPyyiNGJKY0H9A6nH3nDwhOmN6mp0Ne4wS4cyP6TAev6nu84RZXAA/pub?gid=1791302643&single=true&output=csv",
+  featuredAmazonCount: 3,
+  featuredWalmartCount: 2,
+  currency: "USD"
+};
